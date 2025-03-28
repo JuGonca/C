49c978730d98a3e458de47a14cb46e49b62a395e
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-03-27 22:24:24 -0300

    Update b_1153.c
    Adicionando comentários

--- BeeCrowd/b_1153.c
+++ BeeCrowd/b_1153.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+//Calculo de fatorial
 
 int main()
-{
+{	//Variáveis
 	int number, c, fatorial;
 	
 	printf("Digite um número: \n");
 	scanf("%d", &number);
 	
+	//Inicializando variáveis
 	fatorial = number;
 	c = number-1;
+	
+	//Loop para calcular o fatorial
 	while(c >= 1){
 		fatorial = fatorial * c;
 		c--;
