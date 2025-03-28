fd2dc53afd21e31a5e0e05a2b5fad76e55a2bcf0
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-03-27 22:44:06 -0300

    Update b_3165.c
    Adicionando comentários

--- BeeCrowd/b_3165.c
+++ BeeCrowd/b_3165.c
@@ -10,45 +10,46 @@ int main()
 	printf("Digite um número entre 5 e 1000: ");
 	scanf("%d", &number);
 	
+	//Verifica se o Número informa está dentro da condição
 	if(number >= 5 && number <= 1000){
 		
+		 //Loop para encontrar os primeiros primos gêmeos menores ou iguais a "number"
 		for(int i=number; i>=5; i--){
+			//Chama a função para verificar se os dois números são primos gêmeos
 			if(VerificaNumPrimo(i) && VerificaNumPrimo(i-2)){
 				printf("%d %d", i, i-2);
-				return 0;
+				return 0;//São Primos Gêmeos
 			}
 		}
-		
 	}
 	else
 		printf("Digite um número válido\n");
-	
-	
-	//Verifica se o number é maior que 1
-	
+
 	return 0;
 }
 
+//Função para verificar se um número é primo
 int VerificaNumPrimo(int number)
 {
+	//Variáveis usadas
 	double raiz;
 	int c;
 	c = 3;
 	raiz = sqrt(number);
 	
-		//Verifica se a divisão por 2 é diferente de 0
+		//Verifica se a divisão por 2 é resulta em 0
 		if(number == 2) 
-			return 1;
+			return 1;//É primo
 		
+		//Verifica se a divisão por 2 é diferente de 0
 		else if(number % 2 != 0){
 			do{
 				//Verifica se number é divisivel por c
 				if(number % c ==0){
-					return 0;
-					//Não é primo
+					return 0;//Não é primo
 				}
 				c = c + 2;
-			}while(c <= raiz);//Repete o loop enquanto c por menor ou igual a raiz de number
+			}while(c <= raiz);//Repete o loop enquanto c for menor ou igual a raiz de number
 			return 1; //É primo
 		}
 		else
