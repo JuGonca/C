9594289c432f92ede73568a3071dab1afa60ff83
JuGonca <86499416+JuGonca@users.noreply.github.com>
2025-03-27 22:34:37 -0300

    Update b_1936.c
    Adicionando comentários

--- BeeCrowd/b_1936.c
+++ BeeCrowd/b_1936.c
@@ -1,30 +1,36 @@
 /*Fatorial*/
+/* Programa para encontrar o menor número de fatoriais cuja soma seja igual a N.*/
+
 #include <stdio.h>
 #include <math.h>
 
 int main(){
+	//Criando variáveis
 	double N, Nf, c, resp;
 	
 	printf("Digite um número: ");
 	scanf("%lf", &N);
 	
-	c = N;
-	resp = 0;
+	//Inicializando variáveis
+	c = N; //Começamos com o maior número possível
+	resp = 0; //Contador de quantos fatoriais foram usados
 	
+	//Loop pricipal: continua até N ser reduzido a zero (Para evitar loops eternos)
 	while(N > 0){
 		
+		//Nf vai ser igual ao fatorial de C (Número atual)
 		Nf = tgamma(c + 1);
 		
+		//Loop para verificar o maior número fatorial que seja próximo ao valor de N
 		while(Nf > N && c > 0){
-				c--;
+				c--;//Diminui o C até encontrar um valor válido
 				Nf = tgamma(c + 1);
-			
 		}
+		//// Subtrai esse fatorial de N (reduzindo N)
 		N = N - Nf;
+		//Incrementa a quantidade de fatoriais usados
 		resp = resp + 1;
 	}
-	
-	
 	
 	printf ("Resposta: %.0f\n", resp);
 	return 0;
